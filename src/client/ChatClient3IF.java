1c922f46e69c0c269870112876cfc2a106805c22
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-08-31 00:34:49 -0300

    Update ChatClient3IF.java

--- src/client/ChatClient3IF.java
+++ src/client/ChatClient3IF.java
@@ -3,15 +3,7 @@ package client;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Remote interface for client classes
- * A method to receive a string
- * A method to update changes to user list
- * 
- * @author Daragh Walshe 	B00064428
- * RMI Assignment 2		 	April 2015
- *
- */
+
 public interface ChatClient3IF extends Remote{
 
 	public void messageFromServer(String message) throws RemoteException;
