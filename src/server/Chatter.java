94a73c8df4ccd58e2a28bc9883f68e32c27e0ba7
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-09-07 01:48:40 -0300

    Update Chatter.java

--- src/server/Chatter.java
+++ src/server/Chatter.java
@@ -8,14 +8,13 @@ public class Chatter {
 	public String name;
 	public ChatClient3IF client;
 	
-	//constructor
+	//construtor
 	public Chatter(String name, ChatClient3IF client){
 		this.name = name;
 		this.client = client;
 	}
 
 	
-	//getters and setters
 	public String getName(){
 		return name;
 	}
